99a7fbaa41ea1f47d95d7804c6487c77929b64c8
Christopher Mark Gore <cgore@cgore.com>
2014-10-05 23:42:00 -0500

    Removing the old .rb file.

--- lambda.rb
+++
@@ -1,7 +0,0 @@
-I = lambda{|x| x}                  # also ->(x){x}, lambda x. x
-K = lambda{|x| lambda{|y| x}}
-S = lambda{|x| lambda{|y| lambda {|z| x[z][y[z]]}}}  #lambda xyz. x z (y z)
-
-T = K   # lambda x. lambda y. x
-F = K[I]  # lambda x. lambda y. y
-
